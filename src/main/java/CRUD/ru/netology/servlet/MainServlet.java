3fa583d312e2167fbbda22e3696594f2de951eb8
Vasiliy <algoritmick.vm@gmail.com>
2025-05-15 00:27:09 +0300

    refactoring

--- src/main/java/CRUD/ru/netology/servlet/MainServlet.java
+++ src/main/java/CRUD/ru/netology/servlet/MainServlet.java
@@ -8,6 +8,7 @@ import CRUD.ru.netology.service.PostService;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 public class MainServlet extends HttpServlet {
     private PostController controller;
@@ -17,50 +18,59 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
+        final PostRepository repository = new PostRepository();
+        final PostService service = new PostService(repository);
         controller = new PostController(service);
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
-        // если деплоились в root context, то достаточно этого
+
         try {
-            final var path = req.getRequestURI();
-            final var method = req.getMethod();
-            // primitive routing
-            if (method.equals("GET") && path.equals("/api/posts")) {
+            final String path = req.getRequestURI();
+            final String method = req.getMethod();
+
+            if (method.equals("GET") && path.equals(API_POSTS)) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-                // easy way
-                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-                controller.getById(id, resp);
+            if (method.equals("GET") && path.matches(API_POSTS_D)) {
+                Optional<Long> idOpt = parseId(path);
+                if (idOpt.isPresent()) {
+                    controller.getById(idOpt.get(), resp);
+                }
                 return;
             }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if (method.equals("POST") && path.equals(API_POSTS)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-                // easy way
-                final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-                controller.removeById(id, resp);
+            if (method.equals("DELETE") && path.matches(API_POSTS_D)) {
+                Optional<Long> idOpt = parseId(path);
+
+                if (idOpt.isPresent()) {
+                    controller.removeById(idOpt.get(), resp);
+                }
                 return;
             }
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } catch (NotFoundException e) {
-            e.printStackTrace();
+            log(e.getMessage());
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } catch (Exception e) {
-            e.printStackTrace();
+            log(e.getMessage());
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
 
-    private long parseId(String path) {
-        return Long.parseLong(path.substring(path.lastIndexOf(STR) + 1));
+    private Optional<Long> parseId(String path) {
+        try {
+            int idx = path.lastIndexOf(STR);
+            if (idx == -1) return Optional.empty();
+            return Optional.of(Long.parseLong(path.substring(idx + 1)));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
     }
 }
 
